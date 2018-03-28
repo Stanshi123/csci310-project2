c31734ae2ac200e02f46c16adbaa251b95daccd2
student <Chaokang>
2018-03-27 21:33:38 -0700

    partial main page black box test

---
+++ blackboxtest/blackboxTest.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+/opt/apache-tomcat/bin/startup.sh
+cucumber --order random
---
+++ blackboxtest/features/step_definitions/main_page_steps.rb
@@ -0,0 +1,52 @@
+Given(/^User has navigated to localhost and login successfully$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^User is on main Page$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^there is a topic input box$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^placeholder text of input box is "([^"]*)"$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^there is a collage shape input box$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^there is a collage width pixel input$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^there is a collage height pixel input$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^there is a radio button of filter option "([^"]*)"$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^there is a radio button of border option "([^"]*)"$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^there is a radio button of rotation option "([^"]*)"$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^there is a save to history button$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^there is a build collage button$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^Build Collage process is triggered$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
