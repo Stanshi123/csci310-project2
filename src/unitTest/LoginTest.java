b8218942631215d4ed737cef58ef9e744e13ac4b
William Tjandra <wtjandra@usc.edu>
2018-03-31 15:25:27 -0700

    test for user login

--- src/unitTest/LoginTest.java
+++ src/unitTest/LoginTest.java
@@ -1,4 +1,3 @@
-
 package whiteboxtests;
 
 import static org.junit.Assert.assertTrue;
@@ -16,31 +15,38 @@ import servlets.LoginServlet;
 public class LoginTest extends Mockito {
 	
 	private LoginServlet servlet;
+	private static final String USERNAME = "william";
+	private static final String PASSWORD = "william";
+	private static final String WRONG_PASSWORD = "wrong";
+	private static final String EMPTY_STRING = "";
     
     @Test
+    // User should be able to login with the correct credentials
     public void testLoginSuccessfully() throws Exception {
     	HttpServletRequest request = mock(HttpServletRequest.class);
     	HttpServletResponse response = mock(HttpServletResponse.class);
     	
-    	when (request.getParameter("username")).thenReturn("william");
-    	when (request.getParameter("password")).thenReturn("william");
+    	when (request.getParameter("username")).thenReturn(USERNAME);
+    	when (request.getParameter("password")).thenReturn(PASSWORD);
     	when (request.getSession()).thenReturn(mock(HttpSession.class));
     	
     	servlet = new LoginServlet();
     	servlet.doPost(request, response);
     	
     	assertTrue(response != null);
+    	
     	String result = servlet.getResult();
     	assertTrue(result.equals("success"));
     }
     
     @Test
+    // User should not be able to login without the correct password
     public void testLoginFail() throws Exception {
     	HttpServletRequest request = mock(HttpServletRequest.class);
     	HttpServletResponse response = mock(HttpServletResponse.class);
     	
-    	when (request.getParameter("username")).thenReturn("william");
-    	when (request.getParameter("password")).thenReturn("tjandra");
+    	when (request.getParameter("username")).thenReturn(USERNAME);
+    	when (request.getParameter("password")).thenReturn(WRONG_PASSWORD);
     	when (request.getSession()).thenReturn(mock(HttpSession.class));
     	
     	servlet = new LoginServlet();
@@ -52,12 +58,13 @@ public class LoginTest extends Mockito {
     }
     
     @Test
+    // User should not be able to login with at least one empty field
     public void testEmptyParameters() throws Exception {
     	HttpServletRequest request = mock(HttpServletRequest.class);
     	HttpServletResponse response = mock(HttpServletResponse.class);
     	
-    	when (request.getParameter("username")).thenReturn("");
-    	when (request.getParameter("password")).thenReturn("");
+    	when (request.getParameter("username")).thenReturn(EMPTY_STRING);
+    	when (request.getParameter("password")).thenReturn(EMPTY_STRING);
     	
     	servlet = new LoginServlet();
     	servlet.doPost(request, response);
@@ -68,12 +75,13 @@ public class LoginTest extends Mockito {
     }
     
     @Test
+    // User should not be able to login with at least one empty field    
     public void testEmptyUsername() throws Exception {
     	HttpServletRequest request = mock(HttpServletRequest.class);
     	HttpServletResponse response = mock(HttpServletResponse.class);
     	
-    	when (request.getParameter("username")).thenReturn("");
-    	when (request.getParameter("password")).thenReturn("william");
+    	when (request.getParameter("username")).thenReturn(EMPTY_STRING);
+    	when (request.getParameter("password")).thenReturn(PASSWORD);
     	
     	servlet = new LoginServlet();
     	servlet.doPost(request, response);
@@ -84,12 +92,13 @@ public class LoginTest extends Mockito {
     }
     
     @Test
+    // User should not be able to login with at least one empty field    
     public void testEmptyPassword() throws Exception {
     	HttpServletRequest request = mock(HttpServletRequest.class);
     	HttpServletResponse response = mock(HttpServletResponse.class);
     	
-    	when (request.getParameter("username")).thenReturn("william");
-    	when (request.getParameter("password")).thenReturn("");
+    	when (request.getParameter("username")).thenReturn(USERNAME);
+    	when (request.getParameter("password")).thenReturn(EMPTY_STRING);
     	
     	servlet = new LoginServlet();
     	servlet.doPost(request, response);
