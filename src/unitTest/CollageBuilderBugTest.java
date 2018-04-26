c149344afb3ab21ca978b812ea1d9e6aa6213a6e
zifanshi <zifanshi@usc.edu>
2018-04-26 15:42:02 -0700

    Zifan&Peijia WhiteBoxTest for Dimension bug in CollageBuilder

--- src/unitTest/CollageBuilderBugTest.java
+++ src/unitTest/CollageBuilderBugTest.java
@@ -9,9 +9,35 @@ import org.junit.Test;
 import Server.CollageBuilder;
 
 public class CollageBuilderBugTest extends CollageBuilder{
+	@Before
+	public void initialize() {
+		List<BufferedImage> images = new ArrayList<>();
+		for(int i=0; i<30; i++)
+		{
+			URL imageURL = null;
+			try {
+				imageURL = new URL("https://s7d1.scene7.com/is/image/PETCO/puppy-090517-dog-featured-355w-200h-d");
+			}catch(MalformedURLException e) {
+			}
+
+			try{
+				BufferedImage image = ImageIO.read(imageURL);
+				images.add(image);
+			}catch(IOException e){
+			}
+		}
+
+		this.images = images;
+	}
 	@Test
 	public void testCreateFont() {
-//		Font fontCreated = createFont(800, 600, 3);
-//		assertTrue("The font created is an instance of font.", fontCreated instanceof Font);
+		Font fontCreated = createFont(800, 600, 3);
+		assertTrue("The font created is an instance of font.", fontCreated instanceof Font);
+	}
+
+	@Test
+	public void testHeightLargerThanWidth() {
+		BufferedImage collageBuilt = createCollageWithImages(600, 800, true, false, null);
+		assertTrue("The Collage built is an instance of image.", collageBuilt instanceof BufferedImage);
 	}
 }
